6ba73c157efe64759f02d608f850016f3cb4c8a3
Diksha <102464818+Diksha2115@users.noreply.github.com>
2024-01-18 03:53:24 +0530

    Create Objects.js

---
+++ Objects.js
@@ -0,0 +1,9 @@
+let person = {
+    name: "Alice",
+    age: 25,
+    isStudent: true
+};
+
+console.log("Name:", person.name);
+console.log("Age:", person.age);
+console.log("Is Student?", person.isStudent);
